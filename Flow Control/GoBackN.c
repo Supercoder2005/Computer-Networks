c0f128b0dc6fe684163e8a2cca2d2d982df8bdb1
Srinjinee Mitra <mitrasrinjinee12@gmail.com>
2026-02-24 12:16:34 +0530

    qs1 practice

---
+++ Flow Control/GoBackN.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<time.h>
+
+#define MAX_FRAMES 100
+
+int window_size,total_frames;
+
+void send_frames(int start,int end){
+    for(int i=start;i<end;i++){
+        printf("\n Sending Frame %d ",i);
+    }
+}
+
+int main(){
+    srand(time(NULL));
+    printf("\n Enter the window size :");
+    scanf("%d",&window_size);
+    printf("\n Enter the total no of frames to send :");
+    scanf("%d",&total_frames);
+    if(total_frames > MAX_FRAMES || window_size > total_frames){
+        printf("\n Invalid input ! Window size must be less than or equal to total frames .");
+        return 1;
+    }
+    int base = 0;
+    int end;
+    while(base < total_frames){
+        if(base + window_size < total_frames){
+            end = base + window_size;
+        }
+        else{
+            end = total_frames;
+        }
+        send_frames(base,end);
+
+        int ack = base + (rand() % ((end - base) + 1));
+        if(ack>=end){
+            printf("\n All frames from %d to %d acknowledged ",base,end-1);
+            base = ack;
+        }
+        else{
+            printf("\n Acknowledgement lost or frame %d lost ! Resending from frame %d",ack,ack);
+            base = ack;
+        }
+    }
+    printf("\n All frames sent successfully.");
+    return 0;
+}
--- stopAndWaitARQ.c
+++ stopAndWaitARQ.c
@@ -1,61 +1,63 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
-#include<stdbool.h>
 #include<time.h>
+#include<stdbool.h>
 
-#define TIMEOUT 2
+#define TIMEOUT 2 
 
-typedef struct {
+typedef struct{
     int data;
-    int seq_num;
+    int seq_no;
 }Frame;
 
-typedef enum {
+// enum is used for giving particular names to integer values (State 0 --> IDLE, State 1 --> WAITING_FOR_ACK)
+typedef enum{
     IDLE,
     WAITING_FOR_ACK
 }State;
 
+// function to simulate error 
 bool simulate_error(){
-    return rand()%2;
+    return rand()%2; // only generate 0/1 , if returns 1 --> error occured if returns 0 --> no error
 }
 
 int main(){
-    srand(time(NULL));
+    srand(time(NULL)); // seed random function --> always generate random values with a starting point , here the starting point is current time = time(NULL)
     int data[] = {10,20,30,40,50};
     int no_frames = sizeof(data)/sizeof(data[0]);
-    int seq_num = 0;
     State state = IDLE;
+    int seq_no = 0;
     for(int i=0;i<no_frames;i++){
         Frame frame;
         frame.data = data[i];
-        frame.seq_num = seq_num;
-        printf("\nSending Frame : Data %d, Seq_no %d",frame.data,frame.seq_num);
+        frame.seq_no = seq_no;
+        printf("\n Sending Frame : Data %d, Seq_no %d",frame.data,frame.seq_no);
         while(1){
             if(state == IDLE){
                 if(simulate_error()){
-                    printf("\nFrame lost : Data %d, Seq_no %d" ,frame.data,frame.seq_num);
+                    printf("\n Frame Lost : Data %d, Seq_no %d",frame.data,frame.seq_no);
                 }
                 else{
-                    printf("\nFrame sent successfully : Data %d, Seq_no %d",frame.data,frame.seq_num);
+                    printf("\n Frame sent successfully : Data %d, Seq_no %d",frame.data,frame.seq_no);
                     state = WAITING_FOR_ACK;
                 }
             }
             if(state == WAITING_FOR_ACK){
-                sleep(TIMEOUT);
+                sleep(TIMEOUT); // sleep() --> time in unit sec
                 if(simulate_error()){
-                    printf("\nAcknowledgement Lost : Data %d, Seq_no %d" ,frame.data,frame.seq_num);
-                    printf("\n Resending Frame : Data %d, Seq_no %d",frame.data,frame.seq_num);
+                    printf("\n Acknowledgement Lost : Data %d, Seq_no %d",frame.data,frame.seq_no);
+                    printf("\n Resending Frame : Data %d, Seq_no %d",frame.data,frame.seq_no);
                     state = IDLE;
                 }
                 else{
-                    printf("\n Data Received Successfully : Data %d, Seq_no %d",frame.data,frame.seq_num);
-                    seq_num = 1-seq_num;
+                    printf("\n Acknowledged successfully : Data %d, Seq_no %d",frame.data,frame.seq_no);
+                    seq_no = 1-seq_no;
                     state = IDLE;
                     break;
                 }
             }
-        }  
+        }
     }
     return 0;
 }
